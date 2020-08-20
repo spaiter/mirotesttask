09c63a0974c911e74cc167e718e1079e175ac230
anon <anon@spryteam.io>
2020-08-20 20:04:33 +0500

    init commit project initialized by SPRING INITIALIZR via IntelliJ IDEA p.s.: also fixed spring-boot-maven-plugin version in pom.xml, coz idea can't found it

---
+++ src/main/java/com/miro/api/widgets/testtask/MiroWidgetsRestApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.miro.api.widgets.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiroWidgetsRestApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiroWidgetsRestApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miro/api/widgets/testtask/MiroWidgetsRestApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.api.widgets.testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiroWidgetsRestApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
