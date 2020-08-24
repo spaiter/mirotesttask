0a2707be1f725822fe9cd5e42bcaaf780f6a516d
anon <anon@spryteam.io>
2020-08-25 03:51:19 +0500

    add java docs for entity repository interface

--- src/main/java/com/miro/api/widgets/testtask/repositories/EntityRepository.java
+++ src/main/java/com/miro/api/widgets/testtask/repositories/EntityRepository.java
@@ -3,6 +3,10 @@ package com.miro.api.widgets.testtask.repositories;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Common CRUD interface for any entity.
+ * @param <Entity> Any entity for CRUD operations.
+ */
 public interface EntityRepository<Entity> {
     Optional<Entity> findEntityById(String id);
     List<Entity> findAllEntities();
