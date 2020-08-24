81c5920f5f70f688d902482f025300082377f251
anon <anon@spryteam.io>
2020-08-24 19:04:19 +0500

    refactor response dto

--- src/main/java/com/miro/api/widgets/testtask/dto/WidgetResponseDTO.java
+++ src/main/java/com/miro/api/widgets/testtask/dto/WidgetResponseDTO.java
@@ -4,13 +4,13 @@ package com.miro.api.widgets.testtask.dto;
  * Widget description that will be produced to client via API response.
  */
 public class WidgetResponseDTO {
-    public String id;
-    public Integer xCoordinate;
-    public Integer yCoordinate;
-    public Integer zIndex;
-    public Integer height;
-    public Integer width;
-    public Integer updatedAt;
+    private final String id;
+    private final Integer xCoordinate;
+    private final Integer yCoordinate;
+    private final Integer zIndex;
+    private final Integer height;
+    private final Integer width;
+    private final Integer updatedAt;
 
     public WidgetResponseDTO(String id, Integer xCoordinate, Integer yCoordinate, Integer zIndex, Integer height, Integer width, Integer updatedAt) {
         this.id = id;
@@ -20,5 +20,33 @@ public class WidgetResponseDTO {
         this.height = height;
         this.width = width;
         this.updatedAt = updatedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Integer getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public Integer getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public Integer getZIndex() {
+        return zIndex;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getUpdatedAt() {
+        return updatedAt;
     }
 }
