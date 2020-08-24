de32313610cae471e59e04bd3ffc6fc241fdce09
anon <anon@spryteam.io>
2020-08-24 19:01:18 +0500

    refactor entities

--- src/main/java/com/miro/api/widgets/testtask/entities/CoordinatePointEntity.java
+++ src/main/java/com/miro/api/widgets/testtask/entities/CoordinatePointEntity.java
@@ -4,11 +4,27 @@ package com.miro.api.widgets.testtask.entities;
  * Point, that represents position on Cartesian coordinate system.
  */
 public class CoordinatePointEntity {
-    public Integer x;
-    public Integer y;
+    private Integer x;
+    private Integer y;
 
     public CoordinatePointEntity(Integer x, Integer y) {
         this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
         this.y = y;
     }
 }
--- src/main/java/com/miro/api/widgets/testtask/entities/WidgetEntity.java
+++ src/main/java/com/miro/api/widgets/testtask/entities/WidgetEntity.java
@@ -7,12 +7,12 @@ import java.util.UUID;
  * Widget class, that contains all widget description.
  */
 public class WidgetEntity {
-    public String id;
-    public CoordinatePointEntity coordinatePoint;
-    public Integer zIndex;
-    public Integer height;
-    public Integer width;
-    public Instant updatedAt;
+    private String id;
+    private final CoordinatePointEntity coordinatePoint;
+    private Integer zIndex;
+    private Integer height;
+    private Integer width;
+    private Instant updatedAt;
 
     public WidgetEntity(Integer xCoordinate, Integer yCoordinate, Integer zIndex, Integer height, Integer width) {
         this.id = UUID.randomUUID().toString();
@@ -21,5 +21,61 @@ public class WidgetEntity {
         this.height = height;
         this.width = width;
         this.updatedAt = Instant.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getXCoordinate() {
+        return this.coordinatePoint.getX();
+    }
+
+    public void setXCoordinate(Integer xCoordinate) {
+        this.coordinatePoint.setX(xCoordinate);
+    }
+
+    public Integer getYCoordinate() {
+        return this.coordinatePoint.getY();
+    }
+
+    public void setYCoordinate(Integer yCoordinate) {
+        this.coordinatePoint.setY(yCoordinate);
+    }
+
+    public Integer getZIndex() {
+        return zIndex;
+    }
+
+    public void setZIndex(Integer zIndex) {
+        this.zIndex = zIndex;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
