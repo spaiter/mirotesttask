12e583b102de86de716e8dc648538366ef750162
anon <anon@spryteam.io>
2020-08-25 04:02:20 +0500

    add java docs for entity repository interface

--- src/main/java/com/miro/api/widgets/testtask/repositories/EntityRepository.java
+++ src/main/java/com/miro/api/widgets/testtask/repositories/EntityRepository.java
@@ -4,12 +4,34 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Common CRUD interface for any entity.
+ * Common CRUD interface for any entity repository.
  * @param <Entity> Any entity for CRUD operations.
  */
 public interface EntityRepository<Entity> {
+    /**
+     * Allow to search entity by its ID in repository.
+     * @param id Entity unique ID.
+     * @return Entity by unique id if exists
+     */
     Optional<Entity> findEntityById(String id);
+
+    /**
+     * Allow to get all entities from repository.
+     * @return List of all entities.
+     */
     List<Entity> findAllEntities();
+
+    /**
+     * Allow to save (upsert) entity in repository.
+     * @param entity Any entity to save.
+     */
     void saveEntity(Entity entity);
+
+
+    /**
+     * Allow to remove entity by its ID from repository.
+     * @param id Entity unique ID.
+     * @return True if entity was found and removed successfully, else false.
+     */
     boolean deleteEntityById(String id);
 }
